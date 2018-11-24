215eaf4fdd13875f50298244280eb6f5db2cc780
Ajay Rao <ajayjrao@gmail.com>
2018-11-24 10:50:09 +0530

    Commiting the 1st Ruby Program

---
+++ Ruby Programs/firstRubyPgm.rb
@@ -0,0 +1,9 @@
+puts "I will count my chickens : "
+puts "Hens #{ 25 + 30 / 5 }"
+puts "Cocks #{ 100 - 25 * 3 + 15 }"
+puts "Now I will count the eggs : "
+puts 3 * 10 + 5 * 15 - 4 * 12
+puts "Is it true that 3+2 > 7-5"
+puts 3 + 2 > 7 - 5
+puts "Is it greater : #{5 > 6 - 2 }"
+puts "Is it greater : #{5 >= 7 - 2 }"
